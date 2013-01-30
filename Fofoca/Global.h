20af9669b45710e148d14fb5ae8e784278236250
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-29 22:58:01 -0200

    criando arquivo Global.h

---
+++ Fofoca/Global.h
@@ -0,0 +1,14 @@
+//
+//  Global.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/29/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#ifndef Fofoca_Global_h
+#define Fofoca_Global_h
+
+
+
+#endif
