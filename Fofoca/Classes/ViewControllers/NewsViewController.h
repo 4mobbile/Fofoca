570f9d151d8f14cb6ada0b6dce20785cc7bf4243
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-30 00:03:14 -0200

    criando tela de noticias

---
+++ Fofoca/Classes/ViewControllers/NewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsViewController.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/29/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ Fofoca/Classes/ViewControllers/NewsViewController.m
@@ -0,0 +1,65 @@
+//
+//  NewsViewController.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/29/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "NewsViewController.h"
+
+@interface NewsViewController ()
+
+@end
+
+@implementation NewsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"newsCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 110;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:[tableView indexPathForSelectedRow] animated:NO];
+    [self performSegueWithIdentifier:@"segueSelectedNews" sender:indexPath];
+}
+
+@end
