fd94251b3e1cc14d14ad6ab30c22576dc320ed20
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-29 23:15:05 -0200

    colocando url de noticias e yql de novelas

--- Fofoca/Global.h
+++ Fofoca/Global.h
@@ -6,9 +6,19 @@
 //  Copyright (c) 2013 4Mobile. All rights reserved.
 //
 
-#ifndef Fofoca_Global_h
-#define Fofoca_Global_h
+
+#import <AdSupport/AdSupport.h>
+
+//---------------------------------------------------------------------------------------------------------
+// URL's
+//---------------------------------------------------------------------------------------------------------
+#define K_URL_NEWS @"http://www.google.com/reader/api/0/stream/contents/user/01062876072085304549/label/babado"
+
+#define K_URL_APP_IN_APP_STORE @"https://itunes.apple.com/us/app/babado/id593111986?mt=8"
 
 
 
-#endif
+//---------------------------------------------------------------------------------------------------------
+// YQL
+//---------------------------------------------------------------------------------------------------------
+#define K_YQL_NOVELAS_FUXICO @"http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%20%3D%20%22https%3A%2F%2Fofuxico.terra.com.br%2Fcanais.html%22%20AND%20xpath%20%3D%20%22%2F%2Fdiv%5B%40class%20%3D%20'canal'%5D%22&diagnostics=true"
