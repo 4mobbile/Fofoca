6e17a5cb107170946deca12989dc185434bb32e3
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-30 22:38:50 -0200

    criando parse de EmissoraModel

---
+++ Fofoca/Models/EmissoraModel+Parse.m
@@ -0,0 +1,13 @@
+//
+//  EmissoraModel+Parse.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/30/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "EmissoraModel+Parse.h"
+
+@implementation EmissoraModel (Parse)
+
+@end
