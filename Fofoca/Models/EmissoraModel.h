5cc8b34910df6e62909cda606522074ef88411c4
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-30 22:37:25 -0200

    criando model de emissora

---
+++ Fofoca/Models/EmissoraModel.h
@@ -0,0 +1,16 @@
+//
+//  EmissoraModel.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/30/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmissoraModel : NSObject
+
+@property (nonatomic, copy) NSString *emissoraName;
+@property (nonatomic, copy) NSArray *novelas;
+
+@end
---
+++ Fofoca/Models/EmissoraModel.m
@@ -0,0 +1,13 @@
+//
+//  EmissoraModel.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/30/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "EmissoraModel.h"
+
+@implementation EmissoraModel
+
+@end
