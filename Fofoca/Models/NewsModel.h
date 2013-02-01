540e2634247e51a62c3971c84df4ca5b1dadc154
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-01-31 23:11:01 -0200

    criando model de noticias

---
+++ Fofoca/Models/NewsModel.h
@@ -0,0 +1,21 @@
+//
+//  NewsModel.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/31/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsModel : NSObject
+
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSDate * publishedAt;
+@property (nonatomic, retain) NSString * content;
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic, retain) NSString * link;
+@property (nonatomic, retain) NSString * origin;
+
+@end
---
+++ Fofoca/Models/NewsModel.m
@@ -0,0 +1,21 @@
+//
+//  NewsModel.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 1/31/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "NewsModel.h"
+
+@implementation NewsModel
+
+@dynamic identifier;
+@dynamic title;
+@dynamic publishedAt;
+@dynamic content;
+@dynamic author;
+@dynamic link;
+@dynamic origin;
+
+@end
