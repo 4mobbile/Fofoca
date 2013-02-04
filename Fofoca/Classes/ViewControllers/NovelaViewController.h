a33e7497a0561c3bfd86e056ed6b0b8d366cf3b0
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-02-04 00:13:34 -0200

    adicionando VC de uma Novela

---
+++ Fofoca/Classes/ViewControllers/NovelaViewController.h
@@ -0,0 +1,13 @@
+//
+//  NovelaViewController.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 2/4/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NovelaViewController : UIViewController
+
+@end
---
+++ Fofoca/Classes/ViewControllers/NovelaViewController.m
@@ -0,0 +1,38 @@
+//
+//  NovelaViewController.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 2/4/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "NovelaViewController.h"
+
+@interface NovelaViewController ()
+
+@end
+
+@implementation NovelaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
