4866bed1c0d1ea8e6e82948f51a389f1c181d156
Alexandre Marones <AlexandreMarones@lourdess-MacBook-Air.local>
2013-02-18 21:59:01 -0300

    criando capterVC e tela no storyboard.

---
+++ Fofoca/Classes/ViewControllers/ChapterViewController.h
@@ -0,0 +1,18 @@
+//
+//  ChapterViewController.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 2/18/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChapterViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *chapterTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *chapterImage;
+@property (weak, nonatomic) IBOutlet UITextView *chapterResume;
+
+
+@end
---
+++ Fofoca/Classes/ViewControllers/ChapterViewController.m
@@ -0,0 +1,29 @@
+//
+//  ChapterViewController.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 2/18/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "ChapterViewController.h"
+
+@interface ChapterViewController ()
+
+@end
+
+@implementation ChapterViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
