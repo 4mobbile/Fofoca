e317743a4364976406fe66ca6dc096881fb72136
Alexandre Marones <AlexandreMarones@Alexandre-MacBook-Air.local>
2013-02-28 22:57:35 -0300

    criando VC da lista de famosos.

---
+++ Fofoca/Classes/ViewControllers/CelebritiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CelebritiesViewController.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 2/28/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CelebritiesViewController : UIViewController
+
+@end
