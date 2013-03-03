daf20decb84f09675b27f7788e7d912c01e172d8
henriqueho <henriqueho@gmail.com>
2013-03-03 11:55:29 -0300

    Implementando menu lateral

---
+++ Fofoca/Classes/ViewControllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Fofoca
+//
+//  Created by Henrique Ho Wong on 03/03/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
