3518c92057cb9bb79c9d3a367932209ef532a5eb
Alexandre Marones <AlexandreMarones@Alexandre-MacBook-Air.local>
2013-03-03 10:59:56 -0300

    criando lista de celebridades.

---
+++ Fofoca/Classes/ViewControllers/ListCelebritiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListCelebritiesViewController.h
+//  Fofoca
+//
+//  Created by Alexandre Marones on 3/3/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListCelebritiesViewController : UIViewController
+
+@end
---
+++ Fofoca/Classes/ViewControllers/ListCelebritiesViewController.m
@@ -0,0 +1,38 @@
+//
+//  ListCelebritiesViewController.m
+//  Fofoca
+//
+//  Created by Alexandre Marones on 3/3/13.
+//  Copyright (c) 2013 4Mobile. All rights reserved.
+//
+
+#import "ListCelebritiesViewController.h"
+
+@interface ListCelebritiesViewController ()
+
+@end
+
+@implementation ListCelebritiesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
